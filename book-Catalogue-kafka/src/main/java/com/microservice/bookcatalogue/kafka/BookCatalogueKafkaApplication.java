18dda4036a7db91ca9870d4061a16cf3f6e572fb
Ram Datta <ramdatta.chandra@gmail.com>
2021-09-18 20:15:31 +0900

    Exercise 5. Kafka component to send message in case of important event.

---
+++ book-Catalogue-kafka/src/main/java/com/microservice/bookcatalogue/kafka/BookCatalogueKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.bookcatalogue.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogueKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogueKafkaApplication.class, args);
+	}
+
+}
